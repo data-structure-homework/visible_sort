b02c530d4e2396092b68461408a79c45830131a2
kulib <1615279202@qq.com>
2023-12-06 19:52:16 +0800

    添加初始化函数，debug

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-#include <time.h>
+#include <time.h>       //时间
+#include <thread>       //线程暂停
+#include <chrono>
+#include <windows.h>    //cmd命令库
 using namespace std;
+
+void init(){
+    SetConsoleOutputCP(65001);//设置CMD为UTF8
+
+    HANDLE hOut;
+    COORD bufferSize;
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    bufferSize.X = 100;
+    bufferSize.Y = 60;
+    SetConsoleScreenBufferSize(hOut, bufferSize);//调整CMD缓冲区大小
+
+    TCHAR title[256];
+    HWND hWnd;
+    GetConsoleTitle(title, 256);
+    hWnd = FindWindow(0, title);
+    MoveWindow(hWnd,20,26,1280,720,true);//调整CMD窗口大小
+}
+
 void display(int number[],int number_count){
     for(int i=30;i>0;i--){
         for(int j=0;j<number_count;j++){
@@ -18,6 +39,8 @@ void insertion_sort(int arr[],int len)
 {
     for(int i=1;i<len;++i)
     {
+        cout<<"sucess"<<endl;
+        cout<<"\033c";
         int key=arr[i];
         int j=i-1;
         while((j>=0) && (key<arr[j]))
@@ -25,28 +48,24 @@ void insertion_sort(int arr[],int len)
             arr[j+1]=arr[j];
             j--;
         }
+        display(arr,len);
         arr[j+1]=key;
+        //this_thread::sleep_for(chrono::milliseconds(50)); // 0.2s
     }
 }
 
 int main(int argc, char* argv[])
 {
-    srand(time(nullptr));
-    int number[70];
-    for(int i=0;i<70;i++){
+    init();
+    srand((unsigned)time(NULL));
+    int number[130];
+    for(int i=0;i<130;i++){
         number[i]=rand()%31+1;
     }
-    display(number,70);
+    display(number,130);
     getchar();
-    int arr[30];
-    srand((unsigned)time(NULL));
-    for(int i=0;i<30;++i)
-    {
-        arr[i]=rand()%31+1;
-    }
-    
-    insertion_sort(arr,30);
-
-    display(arr,30);
+    insertion_sort(number,130);
+    getchar();
+    //display(arr,30);
     return 0;
 }
