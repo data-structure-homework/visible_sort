e94e44a6cf1600e2b9b77afb05aeaff94e3628c6
xvan1234 <1007281914@qq.com>
2023-12-06 18:59:19 +0800

    插入排序

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
+
+void insertion_sort(int arr[],int len)
+{
+    for(int i=1;i<len;++i)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while((j>=0) && (key<arr[j]))
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+void display(int arr[],int len)
+{
+    for(int i=0;i<len;++i)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main(int argc, char* argv[])
 {
-    cout<<"hello,world"<<endl;
-    cout<<"THIS IS A TEST"<<endl;
-    system("pause");
-    //test
-    // this is a code, insert it
+    int arr[30];
+    srand((unsigned)time(NULL));
+    for(int i=0;i<30;++i)
+    {
+        arr[i]=rand()%31+1;
+    }
+    
+    insertion_sort(arr,30);
+
+    display(arr,30);
     return 0;
 }
