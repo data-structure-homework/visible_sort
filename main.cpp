0bd8a666f2775bde7647415b323147ed77c6e3f0
Reyes <fzt021379@qq.com>
2023-12-13 18:44:24 +0800

    version name

--- main.cpp
+++ main.cpp
@@ -395,7 +395,7 @@ void Select_sort(int arr[],int len){  //选择排序.
 
 void main_menu(){       //主菜单
     system("cls");
-    cout<<"可视化排序算法v1.4.2"<<endl;
+    cout<<"可视化排序算法v1.4.3"<<endl;
     cout<<"当前选择数组大小:";
     cout<<get_length()<<endl;
     cout<<"1.单排序可视化演示(将自动切换数组大小至100)"<<endl;
